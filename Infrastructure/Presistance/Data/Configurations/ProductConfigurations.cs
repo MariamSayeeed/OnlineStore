86e550986942ccc2390a04baff19225f4a943128
MariamSayeeed <ms1023993979@gmail.com>
2025-04-16 01:12:26 +0200

    Add project files.

---
+++ Infrastructure/Presistance/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistance.Data.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            // Brand Releations
+            builder.HasOne(p => p.ProductBrand)
+                   .WithMany()
+                   .HasForeignKey(p=>p.BrandId);
+
+
+            // Type class Releations
+            builder.HasOne(p => p.ProductType)
+                   .WithMany()
+                   .HasForeignKey(p => p.TypeId);
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+        }
+    }
+}

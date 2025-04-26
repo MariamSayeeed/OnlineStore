fed3c6220f5a4dda6eecc5c292733009fb6370fc
MariamSayeeed <ms1023993979@gmail.com>
2025-04-26 15:31:22 +0300

    Error Handling Middleware

---
+++ OnlineStore/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Shared.ErrorModels;
+
+namespace OnlineStore_Api.Middlewares
+{
+    public class GlobalErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalErrorHandlingMiddleware> _logger;
+
+
+        public GlobalErrorHandlingMiddleware(RequestDelegate next , ILogger<GlobalErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                // Log Exception
+                _logger.LogError(ex , ex.Message);
+                
+                // 1. Set Status Code
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                // 2. Set Content Type  application/json
+                context.Response.ContentType = "application/json";
+
+                // 3. Response Object (Body)
+                var response = new ErrorDetails()
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError,
+                    ErrorMessage = ex.Message
+                };
+
+                // Return Response
+
+                await context.Response.WriteAsJsonAsync(response);
+            
+            }
+        }
+
+    }
+}
